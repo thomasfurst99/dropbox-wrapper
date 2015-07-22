07aeca51c0b13f071899b51124ef9d0c5fc087de
thomasfurst99 <tomasfurst99@gmail.com>
2015-07-22 13:50:40 +0200

    First commit

---
+++ DropboxWrapper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DropboxWrapper
+{
+    static class Program
+    {
+        /// <summary>
+        /// Mutex used to create only single instance app.
+        /// </summary>
+        static Mutex mutex = new Mutex(true, Assembly.GetExecutingAssembly().GetType().GUID.ToString());
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Only one instance of app at time.
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Context());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("Only one instance at time!", "Error");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
